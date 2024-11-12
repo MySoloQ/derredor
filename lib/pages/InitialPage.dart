2f129b7f0d6a8184aad05a0e90da950954d205d9
krStuud <kaio.r.santos8@aluno.senai.br>
2024-11-12 17:57:25 -0300

    adicionando arquivos e alterando códigos

--- lib/pages/InitialPage.dart
+++ lib/pages/InitialPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:derredor/style/style.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Initialpage extends StatefulWidget {
   const Initialpage({super.key});
@@ -23,147 +24,186 @@ class _InitialpageState extends State<Initialpage> {
     }
 
     return Scaffold(
-      body: SafeArea(
+
+      body: Column(
+              children: [
+      Expanded(
+        child: Container(
+            height: height / 2,
+            color: Colors.white,
+            child: Stack(
+              children: [
+                Container(
+                  height: height / 2,
+                  child: CarouselSlider(
+                    options: CarouselOptions(height: height/2,
+                        aspectRatio: 16/9,
+                      viewportFraction: 1.0,
+                      enableInfiniteScroll: true,
+                      autoPlay: true,
+                      autoPlayInterval: Duration(milliseconds: 5000),
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.linearToEaseOut,
+                      enlargeCenterPage: false,
+                    ),
+                    items: [
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('assets/fervedouro_bela_vista_1.png',),
+                          fit: BoxFit.cover)
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                CustomPaint(
+                  size: Size(width, height),
+                  painter: Painter(),
+                ),
+                Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                    image: AssetImage(
+                      'assets/icons/icon_sol_tocantins.png',
+                    ),
+                  ),
+                ))
+              ],
+            )),
+      ),
+      Expanded(
+        child: Container(
+          height: 500,
+          color: StyleApp.bgColor,
           child: Column(
-        children: [
-          Expanded(
-            child: Container(
-                height: height / 2,
-                color: Colors.white,
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage('assets/fervedouro_bela_vista.png')))
+            children: [
+              Text(
+                'Escolha como deseja acessar o app',
+                style: TextStyle(color: Colors.black45, fontSize: 20),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 50, 0, 50),
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  decoration: BoxDecoration(
+                      color: _isPressed
+                          ? Colors.greenAccent
+                          : Colors.red,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: ElevatedButton(
+                    child: Text(
+                      'Já possuo cadastro',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
-                    CustomPaint(
-                      size: Size(width, height),
-                      painter: HeartPainter(),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 50),
+                      backgroundColor: Colors.transparent,
                     ),
-                  ],
-                )),
-          ),
-          Expanded(
-            child: Container(
-              height: 500,
-              color: StyleApp.bgColor,
-              child: Column(
-                children: [
-                  Text(
-                    'Escolha como deseja acessar o app',
-                    style: TextStyle(color: Colors.black45, fontSize: 20),
+                    onPressed: _toggleButton,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 50, 0, 50),
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      decoration: BoxDecoration(
-                          color: _isPressed
-                              ? Color.fromARGB(255, 197, 197, 197)
-                              : StyleApp.detailsColor,
-                          borderRadius: BorderRadius.circular(30)),
-                      child: ElevatedButton(
-                        child: Text(
-                          'Já possuo cadastro',
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(300, 50),
-                          backgroundColor: Colors.transparent,
-                        ),
-                        onPressed: _toggleButton,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: SizedBox(
-                      child: Stack(
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: SizedBox(
+                  child: Stack(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Transform.translate(
-                                offset: Offset(-30, 0),
-                                child: ClipRect(
-                                  child: Container(
-                                    width: 130,
-                                    height: 2,
-                                    color: Color(0xE3E3E3FF),
-                                  ),
-                                ),
+                          Transform.translate(
+                            offset: Offset(-30, 0),
+                            child: ClipRect(
+                              child: Container(
+                                width: 130,
+                                height: 2,
+                                color: Color(0xE3E3E3FF),
                               ),
-                              Text('OU'),
-                              Transform.translate(
-                                offset: Offset(30, 0),
-                                child: ClipRect(
-                                  child: Container(
-                                    width: 130,
-                                    height: 2,
-                                    decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(0.01),
-                                        color: Color(0xE3E3E3FF)),
-                                  ),
-                                ),
-                              )
-                            ],
+                            ),
+                          ),
+                          Text('OU'),
+                          Transform.translate(
+                            offset: Offset(30, 0),
+                            child: ClipRect(
+                              child: Container(
+                                width: 130,
+                                height: 2,
+                                decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.circular(0.01),
+                                    color: Color(0xE3E3E3FF)),
+                              ),
+                            ),
                           )
                         ],
-                      ),
-                    ),
+                      )
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      decoration: BoxDecoration(
-                          color: _isPressed
-                              ? StyleApp.detailsColor
-                              : Color.fromARGB(255, 197, 197, 197),
-                          borderRadius: BorderRadius.circular(30)),
-                      child: ElevatedButton(
-                        child: Text(
-                          'Desejo me cadastrar',
-                          style: TextStyle(fontSize: 20, color: Colors.black45),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                                side:
-                                    BorderSide(color: Colors.black, width: 2)),
-                            minimumSize: Size(300, 50),
-                            backgroundColor: Colors.white),
-                        onPressed: _toggleButton,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  decoration: BoxDecoration(
+                      color: _isPressed
+                          ? Colors.greenAccent
+                          : Colors.red,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: ElevatedButton(
+                    child: Text(
+                      'Desejo me cadastrar',
+                      style: TextStyle(fontSize: 20, color: Colors.black45),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            side:
+                                BorderSide(color: Colors.black, width: 2)),
+                        minimumSize: Size(300, 50),
+                        backgroundColor: Colors.white),
+                    onPressed: _toggleButton,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      )),
+        ),
+      ),
+              ],
+            ),
     );
   }
 }
 
-class HeartPainter extends CustomPainter {
+class Painter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
       ..color = StyleApp.bgColor
       ..style = PaintingStyle.fill;
 
-    final path = Path();
-    path.moveTo(0, size.height);
-    path.cubicTo(size.width * .24, size.height * .60, size.width * .60,
-        size.height * .80, size.width, size.height * 0.65);
-    path.cubicTo(size.width, size.height * .15, size.width, size.height,
+    final pathBottom = Path();
+    pathBottom.moveTo(0, size.height);
+    pathBottom.cubicTo(size.width * .24, size.height * .60, size.width * .60,
+        size.height * .8, size.width, size.height * 0.60);
+    pathBottom.cubicTo(size.width, size.height * .15, size.width, size.height,
         size.width, size.height);
-    path.close();
+    pathBottom.close();
 
-    canvas.drawPath(path, paint);
+    canvas.drawPath(pathBottom, paint);
+
+    final pathTop = Path();
+    pathTop.moveTo(0, size.height * 0.24);
+    pathTop.cubicTo(size.width * .24, size.height * .001, size.width * .60,
+        size.height * .20, size.width, size.height * 0.001);
+    pathTop.cubicTo(size.width, -size.height * .15, -size.width, -size.height,
+        -size.width, -size.height);
+    pathTop.close();
+
+    canvas.drawPath(pathTop, paint);
   }
 
   @override
--- lib/style/style.dart
+++ lib/style/style.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class StyleApp {
   static const Color bgColor = Color(0xFFFFD700);
-  static const Color detailsColor = Color(0xff2d66f3);
+  static const Color detailsColor = Color(0xFF007FFF);
 }
