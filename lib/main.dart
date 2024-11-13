e3bae5e34fdd4b1205b4cd3881967e2fdac262ab
davifontinele <davifontineledevi@gmail.com>
2024-11-13 17:49:13 -0300

    correção da pagina

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:derredor/pages/RegisterPage.dart';
+import 'package:derredor/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,145 @@
+import 'dart:ffi' as ffi;
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:derredor/style/style.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class Registerpage extends StatefulWidget {
+  const Registerpage({super.key});
+
+  @override
+  State<Registerpage> createState() => _RegisterpageState();
+}
+
+List<String> sexo = ['Masculino', 'Feminino', 'Não definido'];
+
+class _RegisterpageState extends State<Registerpage> {
+  @override
+  Widget build(BuildContext context) {
+    double largura = MediaQuery.of(context).size.width;
+    double altura = MediaQuery.of(context).size.height;
+    String sexoSelecionado = '';
+    // String _dropdownValue = 'One';
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                    child: Icon(Icons.person_add_rounded,
+                        color: Style.Azul, size: 130)),
+              ],
+            ),
+            Container(
+              width: largura,
+              height: 60,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Nome:",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      )),
+                ),
+              ),
+            ),
+            Container(
+              width: largura,
+              height: 60,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Sobrenome:",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      )),
+                ),
+              ),
+            ),
+            Container(
+              width: largura,
+              height: 60,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Email:",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      )),
+                ),
+              ),
+            ),
+            sexo.map((item) {
+              return RadioListTile(
+                value: sexoSelecionado,
+                groupValue: item,
+                onChanged: (value) {
+                  sexoSelecionado = value!;
+                  setState(() {});
+                },
+                title: Text("Sexo"),
+              );
+            }).toList(),
+            RadioListTile<String>(
+              title: const Text("Masculino"),
+              value: sexo[0],
+              groupValue: sexoSelecionado,
+              onChanged: (String? value) {
+                setState(() {
+                  sexoSelecionado = value.toString();
+                });
+              },
+            ),
+            Container(
+              width: largura,
+              height: 60,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Usuário:",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      )),
+                ),
+              ),
+            ),
+            Container(
+              width: largura,
+              height: 60,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Senha:",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      )),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(largura / 2, 50),
+                  backgroundColor: Style.Azul,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30))),
+              child: Text("CADASTRAR",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
