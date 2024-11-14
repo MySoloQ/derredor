88ad5329f88a0a1d415f14abf451f52077d5646a
kaiopo <167123648+krStuud@users.noreply.github.com>
2024-11-14 17:43:30 -0300

    ✨ feat: Página inicial da aplicação (#1)
    * criando tela inicial
    
    * alterações na tela inicial
    
    * adicionando arquivos e alterando códigos
    
    * reestilização da tela inicial e programação dos botões
    
    * Desenvolvendo design tela inicial
    
    * Finalizando página incial e configurando família de fontes
    
    ---------
    
    Co-authored-by: krferreiras <dossantos.krf@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
+import 'package:derredor/pages/paginaTeste.dart';
 import 'package:flutter/material.dart';
+import 'package:derredor/pages/InitialPage.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+
   const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'initialPage',
+    routes: {
+      'initialPage': (context) => const Initialpage(),
+      'paginaTeste': (context) => const Paginateste(),
+    },
+  );
+}
 }
