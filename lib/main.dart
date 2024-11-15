416ed93d88e269e82ff39060cc31b9dd6df6ea55
FonteDuvidosa <58407930+davifontinele@users.noreply.github.com>
2024-11-14 21:09:57 -0300

    Adição da página de cadastro (#3)
    * consertando bobajadas
    
    * correção de versão
    
    * atualização da versao pubspec.yaml
    
    * elementos iniciais da pagina de registro
    
    * finalização da pagina de cadastro
    
    * correção da pagina
    
    * correção da lista RadioListTile
    
    * modificação do seletor de sexo
    
    * teste da seleção de sexo
    
    * finalização do wiget de seleção de sexo e otimização do código
    
    * adição do campo de data de nascimento, adição do botão de voltar a página, adição de titulo á página e correção do campo de senha
    
    * adição de subtitulo
    
    ---------
    
    Co-authored-by: krStuud <kaio.r.santos8@aluno.senai.br>

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:derredor/pages/paginaTeste.dart';
 import 'package:flutter/material.dart';
 import 'package:derredor/pages/InitialPage.dart';
+import 'package:derredor/pages/register_page.dart';
+
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-
   const MainApp({super.key});
-
+  
 @override
 Widget build(BuildContext context) {
   return MaterialApp(
@@ -18,6 +19,7 @@ Widget build(BuildContext context) {
     routes: {
       'initialPage': (context) => const Initialpage(),
       'paginaTeste': (context) => const Paginateste(),
+      'registerPage': (context) => Registerpage(),
     },
   );
 }
